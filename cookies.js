38a1e1ce4f2fb4e8b9a48f14eb69e11069497dae
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-14 12:56:01 +0100

    Documentada función para eliminar cookie

--- cookies.js
+++ cookies.js
@@ -1,7 +1,7 @@
 /**
  * Función para obtener una cookie
- * @param  {string} name Recibe el nombre de la cookie
- * @return {string}      Devuelve el valor de la cookie en un string o null
+ * @param  {String} name Recibe el nombre de la cookie
+ * @return {String}      Devuelve el valor de la cookie en un string o null
  */
 function getCookie(name){
     var cname = name + "=";
@@ -20,18 +20,19 @@ function getCookie(name){
 
 /**
  * Establece una cookie mediante los parámetros pasados a la función
- * @param {string} name     Nombre de la cookie.
- * @param {string} value    Valor de las cookies.
- * @param {string} expires  Fecha de caducidad de la cookie (por defecto, el
+ * @param {String} name     Nombre de la cookie.
+ * @param {String} value    Valor de las cookies.
+ * @param {Date} expires  Fecha de caducidad de la cookie (por defecto, el
  *                          final de la sesión).
- * @param {string} path     Camino donde se aplica esta cookie, por defecto
+ * @param {String} path     Camino donde se aplica esta cookie, por defecto
  *                          el dominio del documento que realiza la llamada.
- * @param {string} domain   Dominio para el cual la cookie es válida (por
+ * @param {String} domain   Dominio para el cual la cookie es válida (por
  *                          defecto, el del documento que hace la llamada).
  * @param {Bool} secure     Indica si la trasnmisión de la cookie requiere una
  *                          transmisión segura (HTTPS).
  */
 function setCookie(name, value, expires, path, domain, secure) {
+    // TOFIX → toGMTString() is deprecated, use toUTCString()
     document.cookie = name + "=" + encodeURIComponent(value) +
     ((expires == null) ? "" : "; expires=" + expires.toGMTString()) +
     ((path == null) ? "" : "; path=" + path) +
@@ -39,13 +40,15 @@ function setCookie(name, value, expires, path, domain, secure) {
     ((secure == null) ? "" : "; secure");
 }
 
-
-// Esta es una adaptación de la función de Dorcht para borrar una cookie
-// name - nombre de la cookie
-// [path] - camino de la cookie (debe ser el mismo camino que el especificado al crear la cookie)
-// [domain] - dominio de la cookie (debe ser el mismo dominio que el especificado al crear la cookie)
-// se considera el camino y dominio por defecto si se especifica null o no se proporcionan argumentos
-
+/**
+ * Borrar Cookie con pasando el nombre, path y domain. Estos dos últimos
+ * se considerarán null si no se han pasado.
+ * @param  {String} name   Nombre de la cookie.
+ * @param  {String} path   Camino de la cookie, el mismo camino que el
+ *                         especificado al crear la cookie).
+ * @param  {String} domain Dominio de la cookie, el mismo dominio que el
+ *                         especificado al crear la cookie)
+ */
 function delCookie (name,path,domain) {
   if (getCookie(name)) {
     document.cookie = name + "=" +

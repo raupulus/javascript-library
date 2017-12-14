02713d18d2b23b1bd15933333b46f018cc8b2532
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-14 13:40:45 +0100

    Planteada apertura de ventanas

--- ventanas.js
+++ ventanas.js
@@ -1,3 +1,26 @@
 /*
     Librería de funciones para trabajar sobre ventanas, objeto window
  */
+
+/**
+ * [abrirVentana description]
+ * @param  {[type]} origen [description]
+ * @param  {[type]} titulo [description]
+ * @param  {[type]} ancho  [description]
+ * @param  {[type]} alto   [description]
+ * @param  {[type]} top    [description]
+ * @param  {[type]} left   [description]
+ * @return {[type]}        [description]
+ */
+function abrirVentana(origen, titulo, ancho, alto, top, left) {
+    ventana = window.open(
+        origen,
+        titulo,
+        'width='+ancho+
+        ',height='+alto+
+        ',top='+top+
+        ',left='+left+
+        ',menubar=yes,resizable=yes,location=yes,scrollbars=yes,status=yes,toolbar=yes');
+
+    return ventana;
+}

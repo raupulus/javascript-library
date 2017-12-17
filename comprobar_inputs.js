367a21e7be2adf8ecbb577f445ac506144258896
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-17 02:28:19 +0100

    Añadida función para comprobar nombre

--- comprobar_inputs.js
+++ comprobar_inputs.js
@@ -1,3 +1,8 @@
 /*
     Librería de funciones para limpiar campos de entrada de formularios
  */
+
+function comprobarNombre(nombre) {
+    var patron = RegExp("^[a-záéíóúñ]+", "i");
+    return patron.test(input);
+}

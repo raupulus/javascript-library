4b68df49a4c9787afb25a1657a15eef348b20415
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-17 20:14:17 +0100

    Agregada función para generar un array de números aleatorios

--- operaciones.js
+++ operaciones.js
@@ -5,3 +5,12 @@
 function generarAleatorio() {
     return parseInt((Math.random()+0.01)*20);
 }
+
+function generarAleatorios(cantidad) {
+    var aleatorios = [];
+
+    for (let i=1; i<=cantidad; i++) {
+        aleatorios.push(parseInt((Math.random()+0.01)*20));
+    }
+    return aleatorios;
+}

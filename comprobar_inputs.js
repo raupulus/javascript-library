c7fc546be4f26da0b8d9df3484db4120a5998839
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-14 18:32:27 +0100

    Renombrado nombre de funciones

--- comprobar_inputs.js
+++ comprobar_inputs.js
@@ -2,47 +2,47 @@
     Librería de funciones para limpiar campos de entrada de formularios
  */
 
-function comprobarCadena(cadena) {
+function validarCadena(cadena) {
     var patron = new RegExp("^[a-záéíóúñ]+", "i");
     return patron.test(cadena);
 }
 
-function comprobarCadenas(cadena) {
+function validarCadenas(cadena) {
     var patron = new RegExp("^[a-záéíóúñ]+(( [a-záéíóúñ]+)+)?$", "i");
     return patron.test(cadena);
 }
 
-function comprobarNombre(nombre) {
+function validarNombre(nombre) {
     var patron = new RegExp("^[a-záéíóúñ]+", "i");
     return patron.test(nombre);
 }
 
-function comprobarEmail(email) {
+function validarEmail(email) {
     var patron = new RegExp("^(http(s)?:\/\/)?([w]{3}[\.])?[a-z0-9]+[\.][a-z]{2,3}$");
     return patron.test(email);
 }
 
-function comprobarEdad(edad) {
+function validarEdad(edad) {
     var patron = new RegExp("^[0-9]{1,3}$", "i");
     return patron.test(edad);
 }
 
-function comprobarTelefono(telefono) {
+function validarTelefono(telefono) {
     var patron = new RegExp("^[0-9]{9}$");
     return patron.test(telefono);
 }
 
-function comprobarWeb(web) {
+function validarWeb(web) {
     var patron = new RegExp("^(http(s)?:\/\/)+([w]{3}[\.])+[a-z0-9]+[\.][a-z]{2,3}$");
     return patron.test(web);
 }
 
-function comprobarPassword(password) {
+function validarPassword(password) {
     var patron = new RegExp("^[0-9a-zA-Z\,\._-]+$");
     return patron.test(password);
 }
 
-function comprobarDNI(dni) {
+function validarDNI(dni) {
     var patron = new RegExp("^[0-9]{2}\.[0-9]{3}\.[0-9]{3}-[a-zA-Z]$");
     return patron.test(dni);
 }
